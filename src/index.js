40e0dc24436cad7b4c40b2eae5cf3c60b67760bd
akdombrowski <akdflyinvee73@gmail.com>
2019-09-21 15:07:38 -0500

    basic app created
    initial app

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
+// Firebase App (the core Firebase SDK) is always required and must be listed first
+import * as firebase from "firebase/app";
+
+// Add the Firebase products that you want to use
+import "firebase/auth";
+import "firebase/firestore";
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+  ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
